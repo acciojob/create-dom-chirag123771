c6dfc0b637071d1f3c845fdb80db98ae08175a31
Chirag Agarwal <72219269+chirag123771@users.noreply.github.com>
2024-01-08 02:25:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function createDom(root) {
+ if (typeof root === 'string') {
+    return document.createTextNode(root);
+  }
 
+  const { type, children, attributes } = root;
+  const element = document.createElement(type);
+
+  if (attributes) {
+    for (const [key, value] of Object.entries(attributes)) {
+      element.setAttribute(key, value);
+    }
+  }
+
+  if (children) {
+    for (const child of children) {
+      const childElement = createDom(child);
+      element.appendChild(childElement);
+    }
+  }
+
+  return element;
 }
 
 module.exports=createDom
